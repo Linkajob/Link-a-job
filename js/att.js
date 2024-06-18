cc7d7766f2ce93c0afcc7a43c647cde1f3377f58
janak <dsandeshhh@gmail.com>
2024-06-18 09:33:41 +0545

    .

--- js/att.js
+++ js/att.js
@@ -3,17 +3,20 @@ function submitForm(event) {
 
     // Prepare form data
     var formData = new FormData();
-    formData.append('entry.292968777', document.getElementById('permanent_address_input').value);
-    formData.append('entry.2010175589', document.getElementById('current_address_input').value);
-    formData.append('entry.775396678', document.getElementById('previous_job_input').value);
-    formData.append('entry.1408469120', document.getElementById('current_job_input').value);
-    formData.append('entry.1253649837', document.getElementById('training_input').value);
-    formData.append('entry.1011077798', document.getElementById('job_type_input').value);
+    formData.append('entry.292968777', document.getElementById('fullname_input').value);
+    formData.append('entry.2010175589', document.getElementById('age_input').value);
+    formData.append('entry.775396678', document.getElementById('contact_input').value);
+    formData.append('entry.1408469120', document.getElementById('email_input').value);
+    formData.append('entry.1253649837', document.getElementById('permanent_address_input').value);
+    formData.append('entry.1011077798', document.getElementById('current_address_input').value);
+    formData.append('entry.608045878', document.getElementById('license_input').value);
+    formData.append('entry.2010849993', document.getElementById('computer_knowledge_input').value);
     formData.append('entry.1659501321', document.getElementById('previous_job_input').value);
     formData.append('entry.2020974035', document.getElementById('current_job_input').value);
-    formData.append('entry.1188174776', document.getElementById('email_input').value);
-    formData.append('entry.274539895', document.getElementById('contact_input').value);
+    formData.append('entry.1188174776', document.getElementById('training_input').value);
+    formData.append('entry.274539895', document.getElementById('job_type_input').value);
     formData.append('entry.1603740132', document.getElementById('work_time_input').value);
+    formData.append('entry.664075864', document.getElementById('comments_input').value);
 
     // Perform form submission
     var xhr = new XMLHttpRequest();
